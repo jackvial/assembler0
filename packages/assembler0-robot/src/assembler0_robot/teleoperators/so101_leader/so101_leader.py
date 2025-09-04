9b6ed07702d9cfb12e6fcc40c9ef009cd942c12e
Jack Vial <vialjack@gmail.com>
2025-09-03 20:34:53 -0400

    Its already clear this library depends LeRobot and implements robots and teleoperators with their API

--- packages/assembler0-robot/src/assembler0_robot/teleoperators/so101_leader/so101_leader.py
+++ packages/assembler0-robot/src/assembler0_robot/teleoperators/so101_leader/so101_leader.py
@@ -9,23 +9,23 @@ from lerobot.motors.feetech import (
 )
 
 from lerobot.teleoperators.teleoperator import Teleoperator
-from .config_so101_screwdriver_leader import SO101ScrewdriverLeaderConfig
+from .config_so101_leader import SO101LeaderConfig
 
 logger = logging.getLogger(__name__)
 
 
-class SO101ScrewdriverLeader(Teleoperator):
+class SO101Leader(Teleoperator):
     """
     SO-101 Leader Arm designed by TheRobotStudio and Hugging Face.
     """
 
-    config_class = SO101ScrewdriverLeaderConfig
-    name = "assembler0_so101_screwdriver_leader"
+    config_class = SO101LeaderConfig
+    name = "assembler0_so101_leader"
 
     # Map the leader motor name to the follower motor and action name
     motor_to_action_map = {"gripper": "screwdriver"}
 
-    def __init__(self, config: SO101ScrewdriverLeaderConfig):
+    def __init__(self, config: SO101LeaderConfig):
         super().__init__(config)
         self.config = config
         norm_mode_body = (
